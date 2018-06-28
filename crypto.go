fb520c27310c2dc0570c18efaf2a6779699e8b89
fox.cpp <fox.cpp@disroot.org>
2018-06-28 20:02:29 +0000

    Clean code a bit

--- crypto.go
+++ crypto.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/base64"
+
 	"github.com/twstrike/ed448"
 )
 
@@ -9,7 +10,7 @@ var publicKey [56]byte
 var curve ed448.DecafCurve
 
 func LoadKeys() error {
-	pubEnc := "X/uLlbPShKzadjbEGjok9fyqeNuVDeG8l6IDcBmxO2MSC2Q82og5cFaY2tGJSaAUmn8nYGmXBEc="
+	pubEnc := "Krjuy0kHp0r1ADKDWAjo+odqLNlVCnb4MAmvbyWMezEt3C18LCWVPVV2d8ggYc0f83p7Wyqd3TU="
 	publicKeySlice, pubErr := base64.StdEncoding.DecodeString(pubEnc)
 	if pubErr != nil {
 		return pubErr
