4a2294b1174185e19c00929b430df0948b12ccfa
Hexawolf <hexawolf@cock.li>
2018-08-18 06:21:00 +0300

    Change log rotation strategy

--- logging.go
+++ logging.go
@@ -13,6 +13,10 @@ import (
 )
 
 func rotate(prefix string, suffix string, directory string) {
+	prefix = prefix + "."
+	suffix = "log"
+
+	// List all files in logs/ directory or create a new directory
 	files, err := ioutil.ReadDir(directory)
 	if err != nil {
 		if os.IsNotExist(err) {
@@ -23,33 +27,36 @@ func rotate(prefix string, suffix string, directory string) {
 		}
 	}
 
+	// Collect list of real log files
 	var logs []string
 	for _, i := range files {
 		if strings.Contains(i.Name(), prefix) &&
 			strings.Contains(i.Name(), suffix) {
-				logs = append(logs, i.Name())
+			logs = append(logs, i.Name())
 		}
 	}
 
-	sort.Sort(sort.Reverse(sort.StringSlice(logs)))
+	// Rename existing log
+	sort.Sort(sort.StringSlice(logs))
+	num := 0
 	for _, i := range logs {
-		arr :=  strings.Split(i, ".")
-		num, err := strconv.Atoi(arr[len(arr)-2])
-		if err != nil {
-			log.Fatal(err)
+		arr := strings.Split(i, ".")
+		num, err = strconv.Atoi(arr[len(arr)-2])
+		if err == nil {
+			break
 		}
-		os.Rename(directory + i, directory + prefix + strconv.Itoa(num+1) +
-			suffix)
 	}
+	os.Rename(directory + prefix + suffix,
+		directory + prefix + strconv.Itoa(num+1) + "." + suffix)
 }
 
 var logFile *os.File
 
 func LogInitialize() {
 	log.SetFlags(log.Ldate | log.Ltime | log.LUTC)
-	rotate("sss.", ".log", "logs/")
+	rotate("sss", "log", "logs/")
 	var err error = nil
-	logFile, err = os.OpenFile("logs/sss.0.log",
+	logFile, err = os.OpenFile("logs/sss.log",
 		os.O_CREATE | os.O_RDWR, 0660)
 	if err != nil {
 		log.Panicln(err)
